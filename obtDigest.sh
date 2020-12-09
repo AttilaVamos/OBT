1b950e2cf17b184123501eb62daf62891e0f76e5
Attila Vamos <attila.vamos@gmail.com>
2020-12-09 22:53:41 +0000

    Improve obtDigest.sh on Wed  9 Dec 22:53:41 GMT 2020.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- obtDigest.sh
+++ obtDigest.sh
@@ -1,6 +1,6 @@
 TODAY="$(date +%Y.%m.%d)"
 
-find ~/OBT-*/ -name 'OBT-*' -type f -exec bash -c "echo '{}' ;tail -n 10 '{}'" \; | egrep '^'"${TODAY}"'|OBT'
+#find ~/OBT-*/ -name 'OBT-*' -type f -exec bash -c "echo '{}' ;tail -n 10 '{}'" \; | egrep '^'"${TODAY}"'|OBT'
 
 IFS=$'\n'
 
@@ -8,11 +8,11 @@ fs=$( find ~/OBT-*/ -name 'OBT-*' -type f -print )
 for f in ${fs[@]}
 do
     echo "$f"
-    ls=$( echo " $(tail -n 10 $f | egrep '^'${TODAY} )" )
-    for l in ${ls[@]}
+    lines=$( echo " $(tail -n 10 $f | egrep '^'${TODAY} )" )
+    for line in ${lines[@]}
     do
-        echo "line:$l"
-        b=$( echo "$l" | cut  -f 1,3 )
+        echo "line:$line"
+        b=$( echo "$line" | cut  -f 1,3 )
         #echo "$b"
     done
 
