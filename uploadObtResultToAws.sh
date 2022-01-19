16b22c0080ed55a000fd89444bd7429260d548cf
Attila Vamos <attila.vamos@gmail.com>
2022-01-19 22:18:34 +0000

    Update OBT-117 IP address
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- uploadObtResultToAws.sh
+++ uploadObtResultToAws.sh
@@ -2,7 +2,8 @@
 
 SSH_KEYFILE="~/hpcc_keypair.pem"
 SSH_OPTIONS="-oConnectionAttempts=5 -oConnectTimeout=100 -oStrictHostKeyChecking=no"
-SSH_TARGET="10.240.62.177"
+#SSH_TARGET="10.240.62.177"
+SSH_TARGET="10.240.62.57"
 
 #rsync -va -e "ssh -i ~/AWSSmoketest.pem"  ~/build/bin/OBT-*.txt ec2-user@ec2-3-133-112-185.us-east-2.compute.amazonaws.com:/home/ec2-user/OBT-009/.
 rsync -va -e "ssh -i ${SSH_KEYFILE} ${SSH_OPTIONS}"  ~/build/bin/OBT-*.txt centos@${SSH_TARGET}:/home/centos/${OBT_ID}/
