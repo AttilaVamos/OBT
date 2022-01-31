83eed81c8585788a60829480fbfb683e92def12c
Attila Vamos <attila.vamos@gmail.com>
2022-01-31 14:16:05 +0000

    Fix missing previous day logs if test session is overlap to the next day.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- archiveLogs.sh
+++ archiveLogs.sh
@@ -348,6 +348,11 @@ then
     find /var/log/HPCCSystems/ -name '*'$(date "+%Y_%m_%d")'*.log' -type f -exec \
          zip ${FULL_ARCHIVE_TARGET_DIR}/${ARCHIVE_NAME} '{}' \; >> ${FULL_ARCHIVE_TARGET_DIR}/${ARCHIVE_NAME}.log 
     
+    # If there is any log from yesterday (overlappeds session case) add it to archive
+    # (It can happen only in overlapped case, because otherwise the OBT cleans up everything at the end of a session.)
+    find /var/log/HPCCSystems/ -name '*'$(date -d '-1 day'  "+%Y_%m_%d")'*.log' -type f -exec \
+         zip ${FULL_ARCHIVE_TARGET_DIR}/${ARCHIVE_NAME} '{}' \; >> ${FULL_ARCHIVE_TARGET_DIR}/${ARCHIVE_NAME}.log 
+    
     echo '' >> ${FULL_ARCHIVE_TARGET_DIR}/${ARCHIVE_NAME}.log
 fi
 
