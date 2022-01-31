6a82fc92ac94f3f4a14041fe5a2c0373e0a66297
Attila Vamos <attila.vamos@gmail.com>
2022-01-31 13:49:58 +0000

    Fix "Command not found error" for dumpstacks() function and job queues dump.
    Tested locally.
    
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- preabort.sh
+++ preabort.sh
@@ -2,23 +2,45 @@
 
 dumpstacks()
 {
-  local processName=$1
-  for p in $(pidof ${processName}); do
-    echo "${processName}[${p}] stacks:"
-    sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" $(which ${processName}) ${p}
+    local processName=$1
+    local binPath=$( find /opt/HPCCSystems/ -iname ${processName} -type f 2>&1 )
+    if [[ -n "$binPath" ]]
+    then
+        pids=$(pidof ${processName})
+        if [[ -n $pids ]]
+        then
+            for p in $(pidof ${processName})
+            do
+                echo "${processName}[${p}] stacks:"
+                sudo gdb --batch --quiet -ex "set interactive-mode off" -ex "thread apply all bt" -ex "quit" ${binPath} ${p}
+            done
+        else
+            echo "No pid found for ${processName}"
+        fi
+    else
+            echo "${processName} binary not found."
+    fi
     echo '==============='
-  done
+    echo
 }
 
 echo 'List of processes:'
 ps aux 
-
+echo '==============='
+echo
+ 
 dumpstacks daserver
 dumpstacks esp
 dumpstacks ecl
 dumpstacks eclcc
 
-echo 'job queues meta data:'
-daliadmin . export /JobQueues jq.xml
-cat jq.xml
-echo '***************'
+daliadminPath=$( find /opt/HPCCSystems/ -iname 'daliadmin' -type f 2>&1 )
+if [[ -n "$daliadminPath" ]]
+then
+    echo 'job queues meta data:'
+    eval ${daliadminPath} . export /JobQueues jq.xml
+    cat jq.xml
+else
+    echo "daliadmin not found"
+fi
+echo '***************'    
