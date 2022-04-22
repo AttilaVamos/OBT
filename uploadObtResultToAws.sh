66123bc17fa83a660aa3bc98cb5333d498480bb9
Attila Vamos <attila.vamos@gmail.com>
2022-04-22 20:29:01 +0100

    Store daily disk stat into diskState.log in uploadObtResultToAws.sh before upload it.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- uploadObtResultToAws.sh
+++ uploadObtResultToAws.sh
@@ -8,7 +8,9 @@ SSH_TARGET="10.240.62.57"
 #rsync -va -e "ssh -i ~/AWSSmoketest.pem"  ~/build/bin/OBT-*.txt ec2-user@ec2-3-133-112-185.us-east-2.compute.amazonaws.com:/home/ec2-user/OBT-009/.
 rsync -va -e "ssh -i ${SSH_KEYFILE} ${SSH_OPTIONS}"  ~/build/bin/OBT-*.txt centos@${SSH_TARGET}:/home/centos/${OBT_ID}/
 
-df -h > ~/diskState.log
+date >> ~/diskState.log
+df -h >> ~/diskState.log
+echo "==============================================" >> ~/diskState.log
 
 #rsync -va -e "ssh -i ~/HPCC-Platform-Smoketest.pem"  ~/diskState.log  centos@ec2-35-183-5-250.ca-central-1.compute.amazonaws.com:/home/ec2-user/OBT-010/.
 rsync -va -e "ssh -i ${SSH_KEYFILE} ${SSH_OPTIONS}"  ~/diskState.log  centos@${SSH_TARGET}:/home/centos/${OBT_ID}/
