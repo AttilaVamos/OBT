45b39707ea212d4e4620e3d2b8959151c14e4482
Attila Vamos <attila.vamos@gmail.com>
2023-01-16 16:46:49 +0000

    Decrease LOG_ARCHIVE_DIR_EXPIRE from 30 (days) to 20 to check it is the reason for Unittests are failed on 8.8.x 4sl/1ch (the last in the sequence test).
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-AWS01/settings.sh
+++ OBT-AWS01/settings.sh
@@ -266,7 +266,7 @@ BUILD_DIR_MAX_NUMBER=7   # Not implemented yet
 
 
 # Local log archive
-LOG_ARCHIEVE_DIR_EXPIRE=30 # days
+LOG_ARCHIEVE_DIR_EXPIRE=20 # days
 
 # Remote, WEB log archive
 WEB_LOG_ARCHIEVE_DIR_EXPIRE=20 # days
