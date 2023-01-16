1ef7c3819af6ec0437dcb4b2bb74b645d0a91d48
Attila Vamos <attila.vamos@gmail.com>
2023-01-16 13:42:47 +0000

    Add candidate-8.12.x to scheduler test list.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-AWS01/settings.sh
+++ OBT-AWS01/settings.sh
@@ -44,14 +44,16 @@ then
     )
 else
     # For obtSequencer.sh 
-    BRANCHES_TO_TEST=( 'candidate-8.8.x' 'candidate-8.10.x' 'master' )
+    BRANCHES_TO_TEST=( 'candidate-8.8.x' 'candidate-8.10.x' 'candidate-8.12.x' 'master' )
 
     # For versioning
     RUN_1=("BRANCH_ID=master")
-    RUN_2=("BRANCH_ID=candidate-8.10.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
-    RUN_3=("BRANCH_ID=candidate-8.10.x" "KEEP_VCPKG_CACHE=1")
-    RUN_4=("BRANCH_ID=candidate-8.8.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
-    RUN_5=("BRANCH_ID=candidate-8.8.x" "KEEP_VCPKG_CACHE=1")
+    RUN_2=("BRANCH_ID=candidate-8.12.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
+    RUN_3=("BRANCH_ID=candidate-8.12.x" "KEEP_VCPKG_CACHE=1")
+    RUN_4=("BRANCH_ID=candidate-8.10.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
+    RUN_5=("BRANCH_ID=candidate-8.10.x" "KEEP_VCPKG_CACHE=1")
+    RUN_6=("BRANCH_ID=candidate-8.8.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
+    RUN_7=("BRANCH_ID=candidate-8.8.x" "KEEP_VCPKG_CACHE=1")
 
     RUN_ARRAY=(
         RUN_1[@]
@@ -59,6 +61,8 @@ else
         RUN_3[@]
         RUN_4[@]
         RUN_5[@]
+        RUN_6[@]
+        RUN_7[@]                
     )
 fi
 #
