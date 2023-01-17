d6adf8c36cb5fe26a0c0d2d8917b34eb72fb5e13
Attila Vamos <attila.vamos@gmail.com>
2023-01-17 13:21:03 +0000

    Add 1200 sec timeout to supercopy.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-AWS01/settings.sh
+++ OBT-AWS01/settings.sh
@@ -407,6 +407,7 @@ TEST_4=( "workflow_contingency_8.ecl" "60" )
 TEST_5=( "stepping7d.ecl" "30" )
 TEST_6=( "stepping7e.ecl" "30" )
 TEST_7=( "stepping7f.ecl" "30" )
+TEST_8=( "supercopy.ecl" "1200" )
 
 
 TIMEOUTS=( 
@@ -417,6 +418,7 @@ TIMEOUTS=(
     TEST_5[@]
     TEST_6[@]
     TEST_7[@]
+    TEST_8[@]
     )
 
 
