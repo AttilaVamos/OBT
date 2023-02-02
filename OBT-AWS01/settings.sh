637fab13f66844574e3066b2f5b4c3b0f13d0cd1
Attila Vamos <attila.vamos@gmail.com>
2023-02-02 11:32:31 +0000

    Remove 8.12.x and master from OBT-AWS01 list.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-AWS01/settings.sh
+++ OBT-AWS01/settings.sh
@@ -44,25 +44,19 @@ then
     )
 else
     # For obtSequencer.sh 
-    BRANCHES_TO_TEST=( 'candidate-8.8.x' 'candidate-8.10.x' 'candidate-8.12.x' 'master' )
+    BRANCHES_TO_TEST=( 'candidate-8.8.x' 'candidate-8.10.x' )
 
     # For versioning
-    RUN_1=("BRANCH_ID=master")
-    RUN_2=("BRANCH_ID=candidate-8.12.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
-    RUN_3=("BRANCH_ID=candidate-8.12.x" "KEEP_VCPKG_CACHE=1")
-    RUN_4=("BRANCH_ID=candidate-8.10.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
-    RUN_5=("BRANCH_ID=candidate-8.10.x" "KEEP_VCPKG_CACHE=1")
-    RUN_6=("BRANCH_ID=candidate-8.8.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
-    RUN_7=("BRANCH_ID=candidate-8.8.x" "KEEP_VCPKG_CACHE=1")
+    RUN_1=("BRANCH_ID=candidate-8.10.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
+    RUN_2=("BRANCH_ID=candidate-8.10.x" "KEEP_VCPKG_CACHE=1")
+    RUN_3=("BRANCH_ID=candidate-8.8.x" "REGRESSION_NUMBER_OF_THOR_CHANNELS=4") 
+    RUN_4=("BRANCH_ID=candidate-8.8.x" "KEEP_VCPKG_CACHE=1")
 
     RUN_ARRAY=(
         RUN_1[@]
         RUN_2[@]
         RUN_3[@]
         RUN_4[@]
-        RUN_5[@]
-        RUN_6[@]
-        RUN_7[@]                
     )
 fi
 #
