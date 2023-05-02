4c6b35fd1fe52ecb9ce710f971449579f65ee1f1
Attila Vamos <attila.vamos@gmail.com>
2023-05-02 18:11:34 +0100

    Apply same settings.sh changes for OBT-010 and OBT-007 as OBT-AWS0x.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-010/settings.sh
+++ OBT-010/settings.sh
@@ -88,7 +88,7 @@ NUMBER_OF_CPUS=$(( $( grep 'core\|processor' /proc/cpuinfo | awk '{print $3}' | 
 SPEED_OF_CPUS=$( grep 'cpu MHz' /proc/cpuinfo | awk '{print $4}' | sort -nru | head -1 | cut -d. -f1 )
 SPEED_OF_CPUS_UNIT='MHz'
 
-BOGO_MIPS_OF_CPUS=$( grep 'bogomips' /proc/cpuinfo | awk '{printf "%5.0f\n", $3}' | sort -nru | head -1 )
+BOGO_MIPS_OF_CPUS=$( grep 'bogomips' /proc/cpuinfo | awk '{printf "%5.0f\n", $3}' | sort -nru | head -1  | tr -d ' ' )
 
 MEMORY=$(( $( free | grep -i "mem" | awk '{ print $2}' )/ ( 1024 ** 2 ) ))
 
