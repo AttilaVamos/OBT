be3341985e4ada1f78ea383e88606503f8a8d81f
Attila Vamos <attila.vamos@gmail.com>
2023-06-14 14:24:44 +0100

    Temporarly set to test to Master and 9.0.x 4s/4c only
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-AWS02/settings.sh
+++ OBT-AWS02/settings.sh
@@ -57,10 +57,10 @@ else
     RUN_ARRAY=(
         RUN_1[@]
         RUN_2[@]
-        RUN_3[@]
-        RUN_4[@]
-        RUN_5[@]
-        RUN_6[@]
+#        RUN_3[@]
+#        RUN_4[@]
+#        RUN_5[@]
+#        RUN_6[@]
     )
 fi
 #
