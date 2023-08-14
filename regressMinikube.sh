e5eca0f1feda62dfc370361c27bc0c0f2cfde573
Attila Vamos <attila.vamos@gmail.com>
2023-08-14 13:37:43 +0100

    Add regressMinikube.sh to testing it in OBT-AWS01.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

---
+++ regressMinikube.sh
@@ -0,0 +1,251 @@
+#!/usr/bin/bash
+
+
+myEcho()
+{
+    msg=$1
+    out=$2
+    [ -z "$out" ] && out=/dev/null
+
+    echo "$msg"
+    echo "$msg" >> $out 2>&1
+}
+
+#set -x;
+
+getLogs=0
+if [ -f ./settings.sh ]
+then
+    . ./settings.sh
+    SOURCE_DIR=$SOURCE_HOME
+    PKG_DIR=$OBT_BIN_DIR/PkgCache
+    SUITEDIR=$TEST_ENGINE_HOME
+    RTE_DIR=$REGRESSION_TEST_ENGINE_HOME
+else
+    SOURCE_DIR="$HOME/HPCC-Platform"
+    SUITEDIR="$SOURCE_DIR/testing/regress/"
+    RTE_DIR="$HOME/RTE-NEWER"
+    #RTE_DIR="$SOURCE_DIR/testing/regress"
+    #RTE_DIR="$HOME/RTE-NEW"
+
+    PKG_DIR="$HOME/HPCC-Platform-build"
+    PKG_IS_DEB=$( type "dpkg" 2>&1 | grep -v -c "not found" )
+    if [[ $PKG_IS_DEB -eq 1 ]]
+    then    
+        PKG_EXT=".deb"
+        PKG_INST_CMD="dpkg -i "
+        PKG_QRY_CMD="dpkg -l "
+        PKG_REM_CMD="dpkg -r "
+    else
+        PKG_EXT=".rpm"
+        PKG_INST_CMD="rpm -i --nodeps "
+        PKG_QRY_CMD="rpm -qa "
+        PKG_REM_CMD="rpm -e --nodeps "
+    fi
+fi
+
+logFile=regressMinikube-$(date +%Y-%m-%d_%H-%M-%S).log 
+#gnome-terminal --title "regressMinikube log" -- bash -c "tail -f -n 200 $logFile" &
+#exec >> ${logFile} 2>&1
+
+#set -x
+INTERACTIVE=0
+FULL_REGRESSION=0
+
+while [ $# -gt 0 ]
+do
+    param=$1
+    param=${param//-/}
+    upperParam=${param^^}
+    echo "Param: ${upperParam}"
+    case $upperParam in
+        I)  INTERACTIVE=1
+            ;;
+               
+        F)  FULL_REGRESSION=1
+            ;;
+    esac
+    shift
+done
+
+echo "INTERACTIVE    : $INTERACTIVE"
+echo "FULL_REGRESSION: $FULL_REGRESSION"
+
+pushd $SOURCE_DIR
+
+git checkout -f master
+git pull upstream master
+git fetch --tags --all
+
+baseTag=$( git tag --sort=-creatordate | egrep 'community' | head -n 1 )
+res=$( git checkout $baseTag  2>&1 )
+echo "res: $res"
+gold=1
+[[ "$baseTag" =~ "-rc" ]] && gold=0
+popd > /dev/null
+
+echo "baseTag: ${baseTag}"
+base=${baseTag##community_}
+[[ $gold -eq 1 ]] && base=${base%-*}
+baseMajorMinor=${base%.*}
+pkg="*community_$baseMajorMinor$PKG_EXT"
+echo "base: ${base}"
+echo "gold:$gold"
+echo "base major.minor:$baseMajorMinor"
+echo "pkg:$pkg"
+CURRENT_PKG=$( ${PKG_QRY_CMD} | grep 'hpccsystems-pl' | awk '{ print $3 }' )
+[ -z "$CURRENT_PKG" ] && CURRENT_PKG="Not installed"
+echo "current installed pkg: $CURRENT_PKG"
+
+CURRENT_PKG_MajorMinor=${CURRENT_PKG%.*}
+echo "current installed pkg major.minor: $CURRENT_PKG_MajorMinor"
+if [[ "$CURRENT_PKG_MajorMinor" == "$baseMajorMinor" ]]
+then
+    echo "The installed platform package is ok to testing cloud."
+else
+    echo "Need to install $pkg to testing cloud."
+    if [[ $INTERACTIVE -eq 1 ]]
+    then
+        candidates=$( find $PKG_DIR -maxdepth 1 -iname $pkg -type f )
+        if [ -n "$candidates" ]
+        then
+            echo "Possible candidate(s):"
+            echo "$candidates"
+        fi
+        exit 1
+    else
+        candidate=$( find $PKG_DIR -maxdepth 2 -iname $pkg -type f | sort -rV | head -n 1 )
+        if [ -n "$candidate" ]
+        then
+            echo "Install $candidate"
+            sudo ${PKG_INST_CMD} $candidate
+            retCode=$?
+            if [[ $retCode -ne 0 ]]
+            then
+                echo "Install $candiadate failed with $retCode."
+                exit 1
+            fi
+            echo "Done."
+        else
+            echo "Platform install package:$pkd not found, exit."
+            exit 1
+        fi
+    fi
+fi
+
+isMinikubeUp=$( minikube status | egrep -c 'Running|Configured'  )
+if [[ $isMinikubeUp -ne 4 ]]
+then
+    echo "Minikube is down, start it up."
+    minikube start
+    
+else
+    echo "Minikube is up."
+fi
+
+helm repo update
+
+helm install minikube hpcc/hpcc --version=$base  -f ./obt-values.yaml
+
+# Wait until everything is up
+tryCount=60
+delay=10
+e=0
+r=0
+while true; do  while read a b c; do r=$(( $r + $a )); e=$(( $e + $b )); printf "%-45s: %s/%s  %s\n" "$c" "$a" "$b"  $( [[ $a -ne $b ]] && echo "starting" || echo "up" ) ; done < <( kubectl get pods | egrep -v 'NAME' | awk '{ print $2 " " $1 }' | tr "/" " "); printf "\nExpected: %s, running %s (%2d)\n" "$e" "$r" "$tryCount"; [[ $r -ne 0 && $r -eq $e ]] && break || sleep ${delay}; tryCount=$(( $tryCount - 1)); [[ $tryCount -eq 0 ]] && break; e=0; r=0; done
+
+sleep 10
+# test it
+printf "\nExpected: %s, running %s (%2d)\n" "$e" "$r" "$tryCount"
+
+if [[ $e -eq $r ]]
+then
+    # Pods are up
+
+    pushd $RTE_DIR
+    echo "cwd: $(pwd)"
+
+    echo "Start ECLWatch."
+    minikube service eclwatch
+    sleep 30
+    
+    uri=$( minikube service list | egrep 'eclwatch' | awk '{ print $8 }' | cut -d '/' -f3 )
+    echo "uri: $uri"
+    ip=$( echo $uri | cut -d ':' -f 1 )
+    echo "ip: $ip"
+    port=$( echo $uri | cut -d ':' -f 2 )
+    echo "port: $port"
+    #echo "Press <Enter> to continue"
+    #read
+
+    echo "Run tests."
+    pwd
+    EXCLUSIONS='--ef pipefail.ecl,soapcall*,httpcall* -e plugin,3rdparty,embedded,python2,spray'
+    
+    CONFIG="./ecl-test-minikube.json"
+    PQ="--pq 2"
+    TIMEOUT="--timeout 1200"
+
+    res=$( ./ecl-test setup --server $ip:$port --suiteDir $SUITEDIR --config $CONFIG  $PQ --timeout 900 --loglevel info 2>&1 )
+    retCode=$?
+    isError=$( echo "${res}" | egrep -c 'Fail ' )
+    echo "retCode: ${retCode}, isError: ${isError}"
+    if [[ ${retCode} -ne 0  || ${isError} -ne 0 ]] 
+    then
+        getLogs=1
+        echo "${res}"
+    else
+        echo "${res}" | egrep 'Suite|Queries|Passing|Failure|Elapsed'
+    fi
+    
+    if [[ $FULL_REGRESSION -eq 1 ]]
+    then 
+        # For full regression on hthor
+        res=$( ./ecl-test run -t hthor --server $ip:$port $EXCLUSIONS --suiteDir $SUITEDIR --config $CONFIG $PQ $TIMEOUT --loglevel info 2>&1 )
+    else
+        # For sanity testing on all engines
+        #res=$( ./ecl-test query --server $ip:$port $EXCLUSIONS --suiteDir $SUITEDIR --config $CONFIG $PQ $TIMEOUT --loglevel info teststdlib* 2>&1 )
+        res=$( ./ecl-test query --server $ip:$port $EXCLUSIONS --suiteDir $SUITEDIR --config $CONFIG $PQ $TIMEOUT --loglevel info alien2.ecl badindex.ecl csvvirtual.ecl fileposition.ecl keydiff.ecl keydiff1.ecl 2>&1 )
+    fi    
+    
+    retCode=$?
+    isError=$( echo "${res}" | egrep -c 'Fail ' )
+    echo "retCode: ${retCode}, isError: ${isError}"
+    if [[ ${retCode} -ne 0  || ${isError} -ne 0 ]] 
+    then
+        getLogs=1
+        echo "${res}"
+    else
+        echo "${res}" | egrep 'Suite|Queries|Passing|Failure|Elapsed'
+    fi
+
+    ./QueryStat2.py  -a -v  -t $ip --port $port --obtSystem=Minikube --buildBranch=$base -p Minikube/ --addHeader --compileTimeDetails 1 --timestamp
+
+    popd
+else
+    echo "Problem with pods start"
+fi
+# Get all logs if needed
+if [[ ${getLogs} -ne 0 ]]
+then
+    echo "Collect logs"
+    dirName="$HOME/shared/Minikube/test-$(date +%Y-%m-%d_%H-%M-%S)"; [[ ! -d $dirName ]] && mkdir -p $dirName; kubectl get pods | egrep -v 'NAME' | awk '{ print $1 }' | while read p; do [[ "$p" =~ "mydali" ]] && param="mydali" || param=""; echo "pod:$p - $param"; kubectl describe pod $p > $dirName/$p.desc;  kubectl logs $p $param > $dirName/$p.log; done; kubectl get pods > $dirName/pods.log;  kubectl get services > $dirName/services.log;  kubectl describe nodes > $dirName/nodes.desc; minikube logs >  $dirName/all.log 2>&1
+else
+    echo "Skip log collection"
+fi
+
+if [[ $INTERACTIVE -eq 1 ]]
+then
+    echo "Testing finished, press <Enter> to stop pods."
+    read
+fi
+
+helm uninstall minikube 
+
+# Wait until everyting is down
+while true; do date; e=0; r=0; while read a b c; do r=$(( $r + $a )); e=$(( $e + $b )); printf "%-45s: %s/%s  %s\n" "$c" "$a" "$b"  $( [[ $a -ne $b ]] && echo "starting" || echo "up" ) ; done < <( kubectl get pods | egrep -v 'NAME' | awk '{ print $2 " " $1 }' | tr "/" " "); printf "\nExpected: %s, running %s\n" "$e" "$r"; [[ $e -eq 0 ]] && break || sleep 10; done; echo "System is down"
+
+minikube stop
+
+echo "End."
+echo "=================================="
