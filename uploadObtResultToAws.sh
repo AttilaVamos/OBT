625f9038d5bc054ad2c6812dbdefd0ff5c5d99c9
Attila Vamos <attila.vamos@gmail.com>
2023-12-08 16:47:53 +0000

    uploadObtResultToAws.sh: Fix typo in checking of 'settings.sh'
    Tested manually on OBT-AWS03.
    
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- uploadObtResultToAws.sh
+++ uploadObtResultToAws.sh
@@ -14,7 +14,7 @@ else
 fi
 
 # If any of those SSH_* parameter is overridden in settings.sh then use them
-[[ -f ./setting.sh ]] && . ./settings.sh
+[[ -f ./settings.sh ]] && . ./settings.sh
 
 #rsync -va -e "ssh -i ~/AWSSmoketest.pem"  ~/build/bin/OBT-*.txt ec2-user@ec2-3-133-112-185.us-east-2.compute.amazonaws.com:/home/ec2-user/OBT-009/.
 rsync -va -e "ssh -i ${SSH_KEYFILE} ${SSH_OPTIONS}"  ~/build/bin/OBT-*.txt centos@${SSH_TARGET}:/home/centos/OBT/${OBT_ID}/
