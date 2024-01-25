1ed6685141ae372dad734aa3da9dd6262a3d99cf
Attila Vamos <attila.vamos@gmail.com>
2024-01-25 13:10:58 +0000

    OBT-AWS02/settings.sh: - Add 'ClassificationTestModified*' to 'ML_GENERATE_ZAP_FOR' because   that test failed on this machine with as well with same skew related   new and occasionally warning.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-AWS02/settings.sh
+++ OBT-AWS02/settings.sh
@@ -602,7 +602,7 @@ ML_TIMEOUT=3600
 ML_PARALLEL_QUERIES=1
 ML_EXCLUDE_FILES="--ef ClassicTestModified.ecl,SVCTest.ecl"
 ML_REGRESSION_EXTRA_PARAM="-fthorConnectTimeout=3600"
-ML_GENERATE_ZAP_FOR=( 'SVTest*' )
+ML_GENERATE_ZAP_FOR=( 'SVTest*' 'ClassificationTestModified*' )
 
 #
 #----------------------------------------------------
