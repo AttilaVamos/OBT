56f99cce57cd8cc4d0dd8d80bd996f59ab6a5992
Attila Vamos <attila.vamos@gmail.com>
2025-05-23 19:27:51 +0100

    OBT.e4p: - Add 'publishCoverageResult.sh' to the project
    publishCoverageResult.sh:
    - Initial check-in.
    - A script to unzip latest coverage result into NginX webserver coverage
      directory and regenerate links to 'latest', 'previous' and 'old' result
      directories.
    
    Tested manually on result servers.
    
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

---
+++ publishCoverageResult.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Publishing latest coverage result start..."
+
+latest=$( find $HOME -iname 'hpcc_cover*.zip' -type f | sort -r | head -n 1 )
+echo "Latest: $latest"
+latestDir=${latest%.zip}
+latestDir=${latestDir##*/}
+echo "Latest dir: $latestDir"
+
+pushd /var/www/html/coverage/ > /dev/null
+
+if [[ ! -d $latestDir ]]
+then
+    echo "Unzip latest result..."
+    res=$( sudo unzip $latest 2>&1 )
+    retCode=$?
+    [[ $retCode -ne 0 ]] && echo "retCode: $retCode"
+    echo "  Done."
+
+    tags=( 'latest' 'previous' 'old' )
+    results=( $(ls -1d *-filt* | sort -r | head -n 3) )
+
+    echo "Remove current links..."
+    sudo rm -v hpcc_coverage-[lop]*
+    echo "  Done."
+
+    echo "Create new links:"
+    i=0
+    for res in ${results[@]}
+    do
+        tag=${tags[i]};
+        printf "\t%d: %s -> %s\n" "$i" "$tag" "$res"
+
+        sudo ln -sf $res hpcc_coverage-$tag
+        i=$(( $i + 1 ))
+    done
+    echo "  Done."
+else
+    echo "$latestDir already published."
+fi
+
+ls -l
+
+popd > /dev/null
+echo "  End."
