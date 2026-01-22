3e725087e7a53699374ad9df3f0daf8bc563fd1e
Attila Vamos <attila.vamos@gmail.com>
2026-01-22 13:55:02 +0000

    OBT-AWS02/settings.sh: - After the Devtoolset-11 globally enabled on OBT-AWS02 (CentOS 7) machine,   remove 'cppembed.ecl' from exclusion list ot see that enable fix the problem   or not.
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- OBT-AWS02/settings.sh
+++ OBT-AWS02/settings.sh
@@ -390,7 +390,8 @@ REGRESSION_EXCLUDE_FILES=""
 #REGRESSION_EXCLUDE_FILES="--excludeFile genjoin2.ecl,genjoin3.ecl"
 
 #REGRESSION_EXCLUDE_CLASS="-e embedded,3rdparty"
-REGRESSION_EXCLUDE_CLASS="-e embedded-r,embedded-js,3rdpartyservice,mongodb,python2 --ef compileOptions.ecl,cppembed.ecl"
+#REGRESSION_EXCLUDE_CLASS="-e embedded-r,embedded-js,3rdpartyservice,mongodb,python2 --ef compileOptions.ecl,cppembed.ecl"
+REGRESSION_EXCLUDE_CLASS="-e embedded-r,embedded-js,3rdpartyservice,mongodb,python2 --ef compileOptions.ecl"
 
 # Exclude spray class from 8.8.x
 if [[ "$BRANCH_ID" == "candidate-8.8.x" ]]
